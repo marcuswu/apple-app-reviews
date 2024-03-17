dfff40e403071db10ac8e3b93ddad03870620729
Marcus Wu <mwu@digitaltorque.com>
2024-03-17 15:01:36 -0400

    Test app file matching

---
+++ backend/updater/reviewupdater_test.go
@@ -0,0 +1,25 @@
+package updater
+
+import "testing"
+
+func TestAppFileMatching(t *testing.T) {
+	var tests = []struct {
+		name         string
+		appId        string
+		expectedFile string
+	}{
+		{"app 1234", "1234", "App-1234.json"},
+		{"app abcd", "abcd", "App-abcd.json"},
+		{"app 1234abc", "1234abc", "App-1234abc.json"},
+		{"app empty", "", "App-.json"},
+	}
+	// The execution loop
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ans := fileForAppId(tt.appId)
+			if ans != tt.expectedFile {
+				t.Errorf("got %s, want %s", ans, tt.expectedFile)
+			}
+		})
+	}
+}
